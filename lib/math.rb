f74441b4f2353baeaf51076c6f5351222ffbe316
Ian Harold <ianhunterharold@gmail.com>
2019-07-26 21:49:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ return summation
 end
 
 def subtraction(num1, num2)
-
+sub = num1 - num2
+return sub
 end
 
 def division(num1, num2)
-
+div = num1/num2
+return div
 end
 
 def multiplication(num1, num2)
-
+mult = num1 * num2
+return mult 
 end
 
 def modulo(num1, num2)
